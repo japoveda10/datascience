c84e410c17924a5210c718a7d0ca816b65e6bdbf
japoveda10 <ja.poveda10@uniandes.edu.co>
2017-07-15 16:37:29 -0500

    Body Mass Index program added

---
+++ Introduction to Python for Data Science/bmi_program.py
@@ -0,0 +1,11 @@
+#Body Mass Index Python Program
+
+print("BMI PYTHON PROGRAM")
+
+height = input("What is your height? \n")
+
+weight = input("What is your weight? \n")
+
+bmi = height / weight ** 2
+
+print("Your Body Mass Index is " + str(bmi))
